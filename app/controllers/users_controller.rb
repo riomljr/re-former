cc1794c4f2da14986e491053e276fefe3e2fdc27
riomljr <22552727+riomljr@users.noreply.github.com>
2023-02-18 19:01:30 -0600

    add edit/update controller and clean code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
  
+  def index 
+    @users = User.all 
+  end
 
   def new
    @user = User.new
@@ -11,12 +14,21 @@ class UsersController < ApplicationController
     if @user.save
       redirect_to new_user_path
     else
-      render :new, status: :unprocessable_entity
+      render :new 
     end
   end
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
 
